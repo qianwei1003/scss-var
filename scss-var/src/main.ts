b63782b3254071b85269ad537780c436415d9588
qianwei1003 <605974929@qq.com>
2025-07-14 13:53:28 +0800

    1

---
+++ scss-var/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+// import './theme-chalk/src/index.scss'
+import './scss/bass.scss'
+import App from './App.vue'
+
+createApp(App).mount('#app')
